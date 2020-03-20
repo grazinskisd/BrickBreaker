7c8876cba416737fcd0d0f7fae0ce040caa100a8
Dalius Grazinskis <grazinskisd@gmail.com>
2020-03-20 12:43:58 +0200

    Elaborate level 3

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrickBreaker
+{
+    public class Spin : MonoBehaviour
+    {
+        public float spinSpeed;
+
+        void Update()
+        {
+            transform.Rotate(Vector3.forward, spinSpeed * Time.deltaTime, Space.Self);
+        }
+    }
+}
